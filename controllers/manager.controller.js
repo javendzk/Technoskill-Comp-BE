a68de58155b92c169195d9340afa21dafc23f851
NahlSyareza <availablearner@protonmail.com>
2024-07-25 20:46:14 +0700

    Cecep commit

--- controllers/manager.controller.js
+++ controllers/manager.controller.js
@@ -1,25 +1,18 @@
 const pg = require("../utils/connect");
 
-exports.register = async function addEmployee(req, res) {
+exports.register = async function register(req, res) {
   // Insert kode REGISTER di sini
-  try {
-    const { name, password } = req.body;
-    const response = await pg.query('INSERT INTO manager (name, password) VALUES ($1, $2) RETURNING *', [name, password]);
-
-    if(response.rows.length === 0) throw new Error('Failed to register');
-
-    res.status(201).json(response.rows[0]);
-  } catch (error) {
-    res.status(500).json(error);
-  }
 };
 
-exports.login = async function addEmployee(req, res) {
+exports.login = async function login(req, res) {
   // Insert kode LOGIN di sini
   try {
     const { name, password } = req.body;
-    const response = await pg.query('SELECT * FROM manager WHERE name = $1 AND password = $2', [name, password]);
-    if(response.rows.length === 0) throw new Error('Failed to login');
+    const response = await pg.query(
+      "SELECT * FROM manager WHERE name = $1 AND password = $2",
+      [name, password]
+    );
+    if (response.rows.length === 0) throw new Error("Failed to login");
     res.status(200).json(response.rows);
   } catch (error) {
     res.status(500).json(error);
