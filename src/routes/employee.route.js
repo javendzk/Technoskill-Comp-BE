9f4a30dbe7eff2693fb3c79cb7e17d3e3cfeae1e
Tianrider <hadiwijayachristian7@gmail.com>
2024-07-31 16:53:08 +0700

    need checkup

--- src/routes/employee.route.js
+++ src/routes/employee.route.js
@@ -1,10 +1,12 @@
-const employeeController = require("../controllers/employee.controller.js");
-const validateMiddleware = require("../middlewares/validate.middleware.js");
-const express = require("express");
+const employeeController = require('../controllers/employee.controller.js');
+const validateMiddleware = require('../middlewares/validate.middleware.js');
+const authMiddleware = require('../middlewares/auth.middleware.js');
+const express = require('express');
 const router = express.Router();
 
-router.post("/login", validateMiddleware, employeeController.login);
-router.get("/getByUid", validateMiddleware, employeeController.getByUid);
-router.put("/UpdateByUid", validateMiddleware, employeeController.UpdateByUid);
+router.post('/login', validateMiddleware, employeeController.login);
+router.get('/getByUid', [authMiddleware, validateMiddleware], employeeController.getByUid);
+router.put('/updateByUid', [authMiddleware, validateMiddleware], employeeController.UpdateByUid);
+router.put('/changePassword', [authMiddleware, validateMiddleware], employeeController.changePassword);
 
 module.exports = router;
