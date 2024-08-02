cf842d7756ae541e4e3a944ac156f69dbebc0d9e
Tianrider <hadiwijayachristian7@gmail.com>
2024-08-02 16:01:31 +0700

    cors?

--- src/configs/cors.config.js
+++ src/configs/cors.config.js
@@ -1,6 +1,6 @@
 const corsOptions = {
-    origin: 'https://tsmanager.vercel.app/', 
-    methods: ['GET', 'POST', 'PUT', 'DELETE'], 
+    origin: 'https://tsmanager.vercel.app',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
 };
 
 module.exports = corsOptions;
